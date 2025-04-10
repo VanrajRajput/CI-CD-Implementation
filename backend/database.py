86a4d0cdc0fc25f384149bbda3e5fb743d0bd7c6
VanrajRajput <pachanbhai72@gmail.com>
2025-04-11 00:38:28 +0530

    uploading

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 load_dotenv()
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/mydb"
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/mydb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
